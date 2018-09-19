819886a4e9b7af8d9257dfb8f396aa8a91d7a7f8
Suu <dvflower@gmail.com>
2018-09-19 17:51:18 +0800

    change url 2 insert0003/zkindlerss

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/mlyksuu/KindleEar.git
+git clone https://github.com/insert0003/zkindlerss.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
