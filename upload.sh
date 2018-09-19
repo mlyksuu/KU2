b0f595ffdb68dd189fc6284a831943feaa7df300
Suu <dvflower@gmail.com>
2018-09-19 18:32:14 +0800

    _(:3

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 rm -rf KindleEar
 git clone https://github.com/insert0003/zkindlerss.git
+mv zkindlerss KindleEar
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
