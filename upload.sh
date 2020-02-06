c7f9f85b8c6b236c1feb2fd7ba896b6df22938b0
mlyksuu <mlyksuu@users.noreply.github.com>
2020-02-07 01:08:46 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/insert0003/zkindlerss.git
-mv zkindlerss KindleEar
+git clone https://github.com/mlyksuu/KindleEar.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
